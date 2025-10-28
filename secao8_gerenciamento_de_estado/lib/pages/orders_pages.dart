b60c37d74c1778d27ef5fd5ee69e477617dae6a2
Eduardo Kern Goes da Silva <edugoes215@gmail.com>
2025-10-28 15:49:31 -0300

    Conclusão Seção 10

--- secao8_gerenciamento_de_estado/lib/pages/orders_pages.dart
+++ secao8_gerenciamento_de_estado/lib/pages/orders_pages.dart
@@ -4,32 +4,10 @@ import 'package:secao8_gerenciamento_de_estado/components/app_drawer.dart';
 import 'package:secao8_gerenciamento_de_estado/components/order.dart';
 import 'package:secao8_gerenciamento_de_estado/models/order_list.dart';
 
-class OrdersPages extends StatefulWidget {
-  const OrdersPages({super.key});
-
-  @override
-  State<OrdersPages> createState() => _OrdersPagesState();
-}
-
-class _OrdersPagesState extends State<OrdersPages> {
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    Provider.of<OrderList> (
-      context,
-      listen: false
-    ).loadOrders().then((_) {
-      setState(() {
-        _isLoading = false;
-      });
-    });
-  }
+class OrdersPages extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    final OrderList orders = Provider.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Meus pedidos'),
@@ -37,15 +15,21 @@ class _OrdersPagesState extends State<OrdersPages> {
         foregroundColor: Colors.white,
       ),
       drawer: AppDrawer(),
-      body: _isLoading ?
-      Center(
-        child: CircularProgressIndicator()
-      )
-      :
-      ListView.builder(
-        itemCount: orders.itemsCount,
-        itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i])
-      )
+      body: FutureBuilder(
+        future: Provider.of<OrderList> (context,listen: false).loadOrders(),
+        builder: (ctx, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return Center(child: CircularProgressIndicator());
+          }else{
+            return Consumer<OrderList>(
+              builder: (ctx, orders, child) => ListView.builder(
+                itemCount: orders.itemsCount,
+                itemBuilder: (ctx, i) => OrderWidget(order: orders.items[i])
+              )
+            );
+          }
+        }
+      ),
     );
   }
 }
